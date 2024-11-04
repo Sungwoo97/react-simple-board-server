4cd72b20bc451086e2bc2aa8a7720f3e4bac1e3e
Sungwoo97 <gwaja97@gmail.com>
2024-11-04 16:56:38 +0900

    11/4 서버 연동

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const port = 8000
 
+//본문에서 넘어온 요청 파싱(변환) 미들웨어를 이용하여
+app.use(express.json()); // json 형식으로 변환  { "name": "Alice", "age": "25" } 
+app.use(express.urlencoded());  // { name: "Alice", age: "25" } 
 
-var mysql      = require('mysql');
-var db = mysql.createConnection({
+var corsOptions = {
+  // origin: 'http://example.com',
+  origin: '*'  //모든 출처를 허용
+}
+app.use(cors(corsOptions));
+
+
+const mysql      = require('mysql');
+const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'react_bbs',
   password : '12345',
@@ -16,7 +27,7 @@ db.connect();
 
 
 
-
+/*
 app.get('/', (req, res) => {
   const sql = "INSERT INTO requested (rowno) VALUES (1)";
   db.query(sql, (err, rows, fields)=> {
@@ -24,7 +35,25 @@ app.get('/', (req, res) => {
     res.send('성공')
     console.log('데이터 추가성공');
   });
-  
+})*/
+
+app.get('/list', (req, res) => {
+  const sql = "SELECT BOARD_ID, BOARD_TITLE, REGISTER_ID, DATE_FORMAT(REGISTER_DATE, '%Y-%m-%d') AS REGISTER_DATE FROM board";
+  db.query(sql, (err, result)=> {
+    if (err) throw err;
+    res.send(result);
+  });
+})
+
+app.post('/insert', (req, res) => {
+  //console.log(req.body);
+  let title = req.body.title;
+  let content = req.body.content;
+  const sql = "INSERT INTO board (BOARD_TITLE, BOARD_CONTENT, REGISTER_ID) VALUES ( ?, ?, 'admin')";
+  db.query(sql, [title, content] ,(err, result)=> {
+    if (err) throw err;
+    res.send(result);
+  });
 })
 
 app.listen(port, () => {
